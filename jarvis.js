c9ee97104c7fb6e510c7bd918034938c34404376
timthornton <t.thornton6@gmail.com>
2012-12-14 21:35:01 -0500

    Added minor documentation

--- jarvis.js
+++ jarvis.js
@@ -1,8 +1,15 @@
 var http = require("http");
 var fs = require('fs');
 
+console.log('Starting Jarvis...');
+
+// Create jarvis server
 http.createServer(function (req, response) {
+
+    // Read in example flac file -> TODO: replace this with voice input feed somehow...
     fs.readFile('test.flac', function (err, data) {
+
+        // When flac is finished loading, POST Google's STT engine
         var options = {
             host: 'www.google.com',
             port: 80,
@@ -19,15 +26,19 @@ http.createServer(function (req, response) {
           res.on('data', function (chunk) {
             var stt = JSON.parse(chunk).hypotheses[0].utterance;
             response.writeHead(200, {'Content-Type': 'text/plain'});
-            response.end(stt);
+            response.end(stt); // Write the stt result to the clients screen
           });
         });
 
+        // Catch http errors
         req.on('error', function(e) {
           console.log('Problem with request: ' + e.message);
         });
 
+        // Write the flac to the request body
         req.write(data);
         req.end();
     });
 }).listen(1337, '127.0.0.1');
+
+console.log('Jarvis successfully started. Listening on 127.0.0.1:1337...');
