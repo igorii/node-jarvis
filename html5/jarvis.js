c59a2a4b9dd6f730d57bc09ea89129c949bb4085
timthornton <t.thornton6@gmail.com>
2012-12-14 23:22:42 -0500

    cleanup

--- html5/jarvis.js
+++ html5/jarvis.js
@@ -1,7 +1,16 @@
-function loadJarvis () {
+function Jarvis = {}
+
+Jarvis.prototype.load = function () {
     var mic = document.getElementById('mic');
     mic.onwebkitspeechchange = function (e) {
         console.log(mic.value);
     }
 }
 
+Jarvis.prototype.query = function (text) {
+    
+};
+
+Jarvis.prototype.say = function(text) {
+ 	
+ }; 
